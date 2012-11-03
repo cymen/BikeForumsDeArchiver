576d8dd76c4566bab1e0817331a003e462f01bf0
Cymen Vig <cymen@8thlight.com>
2012-11-03 12:53:48 -0500

    Support page numbers -- if archive URL has page number go to that page number in regular forum

--- redirect_to_non_archive.js
+++ redirect_to_non_archive.js
@@ -1,8 +1,24 @@
 var matcher = /\/archive\/index.php\/t-/;
-var token = "#redirecting"
+var token = "#redirecting";
+
+var getPageNumber = function(url) {
+  // http://www.bikeforums.net/archive/index.php/t-769181-p-33.html
+  var matches = url.match(/p-([0-9]+)+\.html$/)
+  if (matches) {
+    return matches[matches.length-1];
+  }
+}
 
 if (matcher.test(window.location.href) && window.location.href.indexOf(token) == -1) {
-  var url = window.location.href.replace(matcher, '/showthread.php?t=').replace(/\.html/, '');
+  var url = window.location.href
+    .replace(matcher, '/showthread.php/')
+    .replace(/\.html/, '')
+    .replace(/-p-[0-9]+/, '');
+  var pageNumber = getPageNumber(window.location.href);
+  if (pageNumber) {
+    url += '/page' + pageNumber;
+  }
+
   window.location.href = token;
   chrome.extension.sendRequest({ redirect: url });
 }
